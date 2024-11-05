bb27cfc4aa89a14ee0982869b9fa8a02da5d526b
vishal12321git <vishalpandey7594@gmail.com>
2024-11-05 23:42:58 +0530

    changes in backend

--- server/index.js
+++ server/index.js
@@ -6,20 +6,22 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-// const app = express()
-const allowedOrigins = process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL] // Set this to your frontend deployed URL in production (e.g., Render frontend)
-    : ['http://localhost:3000']; // For local development
-
-// CORS configuration
-app.options('*', cors()); // Pre-flight request handling
-
+// const app = express();
 app.use(cors({
-    origin: allowedOrigins,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+
+// const allowedOrigins = process.env.NODE_ENV === 'production' 
+//     ? [process.env.FRONTEND_URL]  // Production URL (frontend on Render)
+//     : ['http://localhost:3000'];  // Development URL (localhost)
+
+// app.use(cors({
+//     origin: allowedOrigins,  // Allow the frontend URL to make requests
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true  // Allow cookies to be sent with requests
+// }));
 
 app.use(express.json())
 app.use(cookiesParser())
